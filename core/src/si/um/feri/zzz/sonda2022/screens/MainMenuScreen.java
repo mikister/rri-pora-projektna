c56fc6b73388d85ef7e7c6389ac9c7f46b86ab7f
Milan Radojevic <mikister2012@gmail.com>
2022-01-11 22:01:03 +0100

    Add main menu

--- core/src/si/um/feri/zzz/sonda2022/screens/MainMenuScreen.java
+++ core/src/si/um/feri/zzz/sonda2022/screens/MainMenuScreen.java
@@ -5,8 +5,13 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -42,7 +47,45 @@ public class MainMenuScreen extends ScreenAdapter {
     }
 
     public void setupStage() {
+        Table table = new Table();
+        stage.addActor(table);
 
+        table.defaults().pad(5);
+
+        Label title = new Label("Plavajoca Sonda", skin);
+
+        TextButton playButton = new TextButton("Play", skin);
+        playButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new GameScreen(game));
+            }
+        });
+
+        TextButton settingsButton = new TextButton("Settings", skin);
+        settingsButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new SettingsScreen(game));
+            }
+        });
+
+        TextButton exitButton = new TextButton("Exit", skin);
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        table.add(title).pad(10).row();
+        table.add(playButton).row();
+        table.add(settingsButton).row();
+        table.add(exitButton).row();
+
+        table.center();
+        table.setFillParent(true);
+        table.pack();
     }
 
     @Override
