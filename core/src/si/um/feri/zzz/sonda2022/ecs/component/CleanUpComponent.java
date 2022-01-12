e12ecbf8ce954b035fa0cd233e32a64a93c91436
Milan Radojevic <mikister2012@gmail.com>
2022-01-12 11:20:46 +0100

    Add CleanUpComponent

---
+++ core/src/si/um/feri/zzz/sonda2022/ecs/component/CleanUpComponent.java
@@ -0,0 +1,10 @@
+package si.um.feri.zzz.sonda2022.ecs.component;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool.Poolable;
+
+public class CleanUpComponent implements Component, Poolable {
+    @Override
+    public void reset() {
+    }
+}
